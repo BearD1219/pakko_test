58a9e2dc528c0516440eb9d9798666c482c24a70
BearD1219 <61118545+BearD1219@users.noreply.github.com>
2021-04-30 22:49:23 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,27 +3,53 @@ let cards = document.querySelectorAll(".cards__item");
 let width = document.documentElement.offsetWidth;
 let height = document.documentElement.offsetHeight;
 let header = document.querySelector('header');
-let banner = document.querySelector(".banner");
+let body = document.querySelector("body");
+let links = document.querySelectorAll('[data-link]');
+let mobileMenu = document.querySelector('.mobile-menu');
+let nav = document.querySelector('.nav');
+let bannerImg = document.querySelector('.banner__img');
+
 hoverAdd();
 cardsHover();
+scrollAnimate();
 window.addEventListener('resize', function() {
     width = document.documentElement.offsetWidth;
     hoverAdd();
     cardsHover();
+    scrollAnimate();
 });
 
-if (scrollY >= 500) {
-    header.classList.add('scroll');
-    banner.classList.add('scroll');
+for(let link of links) {
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+        if (mobileMenu.classList.contains('close')) {
+            mobileMenu.classList.remove('close');
+            nav.classList.remove('active');
+            body.classList.remove('active');
+        };
+        let linkName = this.getAttribute('data-link');
+        if (linkName == 'top') {
+            window.scrollTo({top: 0, behavior: 'smooth'});
+            return;
+        }
+        let linkElem = document.querySelector(linkName);
+        let elemScroll = linkElem.offsetTop;
+        if (mobileMenu.offsetWidth > 0) {
+            if (bannerImg.offsetWidth > 0) {
+                window.scrollTo({top: (elemScroll - header.offsetHeight + 22), behavior: 'smooth'});
+            } else {
+                window.scrollTo({top: (elemScroll - header.offsetHeight), behavior: 'smooth'});
+            }
+        } else {
+            window.scrollTo({top: (elemScroll - header.offsetHeight + 50), behavior: 'smooth'});
+        }
+    });
 }
-document.addEventListener('scroll', function() {
-    if (scrollY >= 500) {
-        header.classList.add('scroll');
-        banner.classList.add('scroll');
-    } else {
-        header.classList.remove('scroll');
-        banner.classList.remove('scroll');
-    }
+
+mobileMenu.addEventListener('click', function() {
+    this.classList.toggle('close');
+    nav.classList.toggle('active');
+    body.classList.toggle('active');
 })
 
 // <HOVER>
@@ -59,3 +85,21 @@ function cardsHover() {
     }
 }
 // </CARDS HOVER>
+
+// <SCROLL HEADER>
+function scrollAnimate() {
+    if (width <= 768 || (width > 768 && scrollY >= 500)) {
+        header.classList.add('scroll')
+        body.classList.add('scroll');
+    }
+    document.addEventListener('scroll', function() {
+        if (width <= 768 || (width > 768 && scrollY >= 500)) {
+            header.classList.add('scroll');
+            body.classList.add('scroll');
+        } else {
+            header.classList.remove('scroll');
+            body.classList.remove('scroll');
+        }
+    })
+}
+// </SCROLL HEADER>
